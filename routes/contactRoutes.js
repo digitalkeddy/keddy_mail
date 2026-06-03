3508574d8a2f8acd2bbf26dc8f41216a5d885f6c
infotech <infoconwayy@gmail.com>
2026-06-03 18:41:26 +0530

    added mongo uri

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,99 +1,5 @@
 
 
-// import express from "express";
-// import nodemailer from "nodemailer";
-// import Contact from "../models/Contact.js";
-
-// const router = express.Router();
-
-// router.post("/", async (req, res) => {
-//   try {
-//     const { name, email, phone, service, message } = req.body;
-
-//     // Save form data to DB
-//     await Contact.create({
-//       name,
-//       email,
-//       phone,
-//       // service,
-//       message,
-//     });
-
-//     // Nodemailer transporter
-//     const transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         user: process.env.MAIL_USER,
-//         pass: process.env.MAIL_PASS,
-//       },
-//     });
-
-//     // Send mail to owner/admin
-//     await transporter.sendMail({
-//       from: process.env.MAIL_USER,
-//       to: process.env.MAIL_USER,
-
-//       subject: `New Tour Inquiry - ${name}`,
-
-//       html: `
-//         <div style="font-family: Arial, sans-serif; padding:20px; background:#f4f4f4;">
-          
-//           <div style="max-width:600px; margin:auto; background:#ffffff; padding:30px; border-radius:10px;">
-
-//             <h2 style="margin-bottom:20px; color:#222;">
-//               New Tour & Travel Enquiry
-//             </h2>
-
-//             <table style="width:100%; border-collapse:collapse;">
-
-//               <tr>
-//                 <td style="padding:10px; font-weight:bold;">Name:</td>
-//                 <td style="padding:10px;">${name}</td>
-//               </tr>
-
-//               <tr style="background:#f9f9f9;">
-//                 <td style="padding:10px; font-weight:bold;">Email:</td>
-//                 <td style="padding:10px;">${email}</td>
-//               </tr>
-
-//               <tr>
-//                 <td style="padding:10px; font-weight:bold;">Phone:</td>
-//                 <td style="padding:10px;">${phone}</td>
-//               </tr>
-
-             
-//               <tr>
-//                 <td style="padding:10px; font-weight:bold;">Message:</td>
-//                 <td style="padding:10px;">${message}</td>
-//               </tr>
-
-//             </table>
-
-//           </div>
-
-//         </div>
-//       `,
-//     });
-
-//     res.status(200).json({
-//       success: true,
-//       message: "Inquiry submitted successfully",
-//     });
-
-//   } catch (err) {
-//     console.log(err);
-
-//     res.status(500).json({
-//       success: false,
-//       message: "Something went wrong",
-//     });
-//   }
-// });
-
-// export default router;
-
 
 import express from "express";
 import nodemailer from "nodemailer";
@@ -180,13 +86,25 @@ router.post("/", async (req, res) => {
       message: "Quote request submitted successfully",
     });
 
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: "Something went wrong",
-    });
   }
+  //  catch (err) {
+  //   console.log(err);
+  //   res.status(500).json({
+  //     success: false,
+  //     message: "Something went wrong",
+  //   });
+  // }
+  catch (err) {
+  console.error("QUOTE ERROR:", err);
+
+  res.status(500).json({
+    success: false,
+    message: err.message,
+    error: process.env.NODE_ENV !== "production"
+      ? err.stack
+      : undefined
+  });
+}
 });
 
 export default router;
